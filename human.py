e5a184560de4ae97327d1ec625a16818d970fbb2
ipap0 <alkaz06@yandex.ru>
2025-12-02 12:19:00 +0300

    update API +fixed constructor for Human +celebrate

--- human.py
+++ human.py
@@ -1,24 +1,27 @@
 import uuid
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class Human():
-    id: int
-    name: str
-    age: int
-
-
+class Human(BaseModel):
+    name: str=None
+    age: int=Field(ge=1, lt=123, description='возраст бывает от 1 до 123')
+    id: str =None
 
     def __init__(self, *args, **kwargs) -> None:
-
-        self.name = args[0]
-        self.age = args[1]
-        self.id = uuid.uuid4()
+        super().__init__(**kwargs)
+        # self.name = name
+        # self.age = age
+        if self.id is None:
+            self.id = uuid.uuid4()
 
     def __str__(self):
         return f'Human ({self.id}, {self.name}, {self.age})'
 
     def __repr__(self):
         return self.__str__()
+
+    def celebrate_birtsday(self):
+        self.age+=1
+        return f'ура, мне {self.age}'
--- main.py
+++ main.py
@@ -1,38 +1,89 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from fastapi.params import Body
 from starlette import status
 from starlette.responses import JSONResponse
 
 from human import Human
 
 #как бы "база данных"
-human_storage = [ Human('Вася ',36), Human('Ира', 29)]
+human_storage = [Human(name='Вася ', age=36), Human(name='Ира',age= 29)]
 print(human_storage)
+
+
 def find_human_by_id(id: str):
     for human in human_storage:
         if str(human.id) == id:
             return human
     return None
 
+
+def filter_human_by_age(humans, age_from=None, age_to=None) -> list[Human]:
+    res = humans
+    if age_from is not None:
+        res = [h for h in res if h.age >= age_from]
+    if age_to is not None:
+        res = [h for h in res if h.age <= age_to]
+    return res
+
+
 app = FastAPI()
+
 
 #заглавная страница
 @app.get('/')
 def index():
     return {'message': 'начало'}
+
+
 #получение всех людей                        GET
 @app.get('/humans')
-def get_humans():
-    return human_storage
+def get_humans(age_from: Optional[int] = None, age_to: Optional[int] = None):
+    print(age_from, age_to)
+    humans = filter_human_by_age(human_storage, age_from, age_to)
+    return humans
+
+
 #получение одного конкретного человека       GET
 @app.get('/humans/{id}')
 def get_one_human(id: str):
     print(id)
     h = find_human_by_id(id)
     if h is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,  content={'message': 'Нет такого человека'})
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Нет такого человека'})
     return h
 #добавление человека                         POST
+@app.post('/humans')
+def add_one_human(new_h : Human = Body()):
+    print(new_h)
+    human_storage.append(new_h)
+    return {'message': 'Добавлен человек', 'human':new_h}
 
 #удаление одного конкретного человека        DEL
-
+@app.delete('/humans')
+def delete_human(id: str):
+    h = find_human_by_id(id)
+    if h is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Нет такого человека'})
+    human_storage.remove(h)
+    return {'message': 'Удален человек', 'human':h}
 #редактирование человека                     PUT
+@app.route('/humans', methods=['PUT'])
+def edit_human(h : Human = Body()):
+    print(f'пришел объект {h}')
+    old_h = find_human_by_id(h.id)
+    if old_h is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Нет такого человека'})
+    else:
+        old_h.name = h.name
+        old_h.age  = h.age
+    return {'message': 'Изменен человек', 'human':h}
+
+@app.get('/celebrate/{id}')
+def celebrate(id: str):
+    h = find_human_by_id(id)
+    if h is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Нет такого человека'})
+    s = h.celebrate_birtsday()
+    return {'message':s, 'human': h}
